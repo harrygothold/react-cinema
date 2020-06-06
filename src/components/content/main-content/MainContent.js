c24a8cd7e066fa3fc8a88cb7d1b1e1a7a5c851c8
Harry Gothold <harrygot@hotmail.com>
2020-06-07 00:05:45 +0100

    Added Pagination component

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -1,25 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MainContent.scss';
 import Slideshow from '../Slideshow/Slideshow';
+import Pagination from '../pagination/Pagination';
+import images from './images';
 
 const MainContent = () => {
-  const images = [
-    {
-      url: 'https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-    },
-    {
-      url: 'https://htmlcolorcodes.com/assets/images/html-color-codes-color-tutorials-hero-00e10b1f.jpg'
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&w=1000&q=80'
+  const [currentPage, setCurrentPage] = useState(1);
+  const paginate = (type) => {
+    if (type === 'prev' && currentPage >= 1) {
+      setCurrentPage((prev) => prev - 1);
+    } else {
+      setCurrentPage((prev) => prev + 1);
     }
-  ];
+  };
   return (
     <div className="main-content">
       <Slideshow images={images} auto={true} showArrows={true} />
       <div className="grid-movie-title">
         <div className="movieType">Now Playing</div>
-        <div className="paginate">Pagination</div>
+        <div className="paginate">
+          <Pagination paginate={paginate} currentPage={currentPage} totalPages={10} />
+        </div>
       </div>
       {/* Grid Component */}
     </div>
