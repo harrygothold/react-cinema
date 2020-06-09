91ea936f2fc0d1f70cda7042ccc7b11514f8fce4
Harry Gothold <harrygot@hotmail.com>
2020-06-09 22:25:36 +0100

    Added details page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Details from './components/content/details/Details';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Header />
-      <div className="app">
-        <Main />
-      </div>
+      <Router>
+        <Header />
+        <div className="app">
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/:id/:name/details" component={Details} />
+          </Switch>
+        </div>
+      </Router>
     </Provider>
   );
 };
