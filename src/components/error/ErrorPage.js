7d09e5963890f5522bd929ff8a8b5119de76743b
Harry Gothold <harrygot@hotmail.com>
2020-06-10 21:18:22 +0100

    Added error component

--- src/components/error/ErrorPage.js
+++ src/components/error/ErrorPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './ErrorPage.scss';
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>Page does not exist</h1>
+    <div className="error-page">
+      <h1 className="error-header">Oops! </h1>
+      <p className="error-msg">Something went wrong</p>
+      <Link to="/">
+        <i className="icon-home"></i> Go back to home page
+      </Link>
     </div>
   );
 };
